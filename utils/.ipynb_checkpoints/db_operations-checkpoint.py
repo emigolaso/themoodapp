aa2bcf4a2be11fe2b64fceb4289e77b35adf5df1
emigolaso <emilio_F1@yahoo.com>
2024-09-12 08:34:30 -0400

    making database name dynamic in db_operations.py

--- utils/.ipynb_checkpoints/db_operations-checkpoint.py
+++ utils/.ipynb_checkpoints/db_operations-checkpoint.py
@@ -10,11 +10,13 @@ load_dotenv()
 # Your Supabase API URL and key
 SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_API_KEY = os.getenv('SUPABASE_API_KEY')
+##This is for the heroku deployment... if you're trying to test locally.. you need to set it in the env file or just straight up add it here
+SUPABASE_DB = os.getenv('SUPABASE_DB')
 
 # Function to insert data into Supabase
 def insert_data_to_supabase(data_string):
     dsl = re.split(",",data_string)
-    url = f"{SUPABASE_URL}/rest/v1/test"
+    url = f"{SUPABASE_URL}/rest/v1/{SUPABASE_DB}
     
     headers = {
         "apikey": SUPABASE_API_KEY,
